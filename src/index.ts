135e083582a1b446cdad96a8bd1daa518c9dea0f
Dan <daniel.bolger@acuris.com>
2020-08-27 16:18:30 +0100

    converted API to use graphQL (via apollo server)

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 import express from "express";
-import { listings } from "./listings";
-import bodyParser from "body-parser";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
+
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get("/listings", (_req, res) => res.send(listings));
-
-app.get("/listings/:id", (req, res) => {
-  const { id } = req.params;
-  const listing = listings.find((listing) => listing.id === id);
-  res.send(listing ? listing : `Unable to find a listing with ID ${id}`);
-});
-app.post("/delete-listing", (req, res) => {
-  const { id } = req.body;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  res.send(`Failed to delete listing with id: ${id}`);
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
